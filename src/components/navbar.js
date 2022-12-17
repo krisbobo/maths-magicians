1797167a1e5322e2d0734712c1f13006fd03fbf5
krisbobo <kiyongbohbo@gmail.com>
2022-12-17 15:00:48 +0100

    Change function name to use caps

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const navbar = () => (
+const Navbar = () => (
   <div>
     <nav>
       <ul>
@@ -13,4 +13,4 @@ const navbar = () => (
   </div>
 );
 
-export default navbar;
+export default Navbar;
